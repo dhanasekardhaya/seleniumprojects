e0ad4b645e7c6b57f64cf0e77b1c5f054ad87491
dhanasekardhaya <dhanasekardhaya@gmail.com>
2025-02-04 23:10:39 -0500

    seleninum

---
+++ src/org/day1/UrlLanuch.java
@@ -0,0 +1,17 @@
+package org.day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class UrlLanuch {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "D:\\Java_seleninum\\Seleniumprojects\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.greenstechnologys.com/");
+
+	}
+
+}
